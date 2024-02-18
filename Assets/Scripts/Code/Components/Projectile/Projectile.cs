01725ad90ebf798b46f3faf2dbf7cc2e326f1ff8
FlavioNovati <f.novati10@gmail.com>
2024-02-18 20:00:27 +0100

    Projectile

---
+++ Assets/Scripts/Code/Components/Projectile/Projectile.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private Movement MovementComponent;
+    [SerializeField] private float Damage = 1f;
+    [SerializeField] private Transform VFX_OnDeath;
+    [SerializeField] private SpriteRenderer ProjectileSprite;
+
+    private Vector2 Direction = Vector2.zero;
+
+    private void FixedUpdate()
+    {
+        MovementComponent.SetDirection(Direction);
+    }
+
+    public void SetDirection(Vector2 dir)
+    {
+        Direction = dir;
+        if(Direction.x < 0)
+        {
+            ProjectileSprite.flipX = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(VFX_OnDeath, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
