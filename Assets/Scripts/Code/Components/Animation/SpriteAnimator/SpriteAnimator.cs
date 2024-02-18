67bd31776cc8cfa8f899281dce430b837b75b121
FlavioNovati <f.novati10@gmail.com>
2024-02-18 17:40:10 +0100

    Sprite Animator

---
+++ Assets/Scripts/Code/Components/Animation/SpriteAnimator/SpriteAnimator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer Renderer;
+    [SerializeField] private Sprite[] SpriteList;
+    [SerializeField] private int Framerate = 30;
+    [SerializeField] private bool Loop = true;
+    [SerializeField] private bool DestroyOnEnd = true;
+    //frame time
+    private float Frequency;
+
+    private void Awake()
+    {
+        Frequency = 1 / (float)Framerate;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Animate());
+    }
+    
+    IEnumerator Animate()
+    {
+        int i = 0;
+        do
+        {
+            while (i < SpriteList.Length)
+            {
+                Renderer.sprite = SpriteList[i];
+                i++;
+                yield return new WaitForSeconds(Frequency);
+            }
+            i = 0;
+        } while (Loop);
+        if (DestroyOnEnd)
+            Destroy(this.gameObject);
+    }
+}
